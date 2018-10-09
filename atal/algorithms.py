49a32784efd1b6693b2b468d62ffc169709fdb8d
José Ivan <jose.ivan.junior@ccc.ufcg.edu.br>
2018-10-09 11:47:29 -0300

    implementação do quick sort para otimizar o algoritmo de ordenação dado, melhorando o algoritmo anterior.

--- atal/algorithms.py
+++ atal/algorithms.py
@@ -1,8 +1,33 @@
 # Esse metodo recebe uma lista com as matriculas dos alunos
 # e retorna essa lista em ordem crescente de matriculas
+
 def retorna_matriculas_decrescente(alist):
-	for j in xrange(len(alist)):
-		for i in xrange(len(alist)-1):
-			if alist[i] > alist[i+1]:
-				alist[i], alist[i+1] = alist[i+1], alist[i]
+	left_index = 0
+	right_index = len(alist) -1
+	quick(alist, left_index, right_index)
 	return alist
+	
+def quick(alist, left_index, right_index):
+	if left_index < right_index:
+		piv = partition(alist, left_index, right_index)
+		quick(alist, left_index, piv - 1)
+		quick(alist, piv + 1, right_index)
+		
+def partition(alist, left_index, right_index):
+	i = left_index - 1
+	pivot = alist[right_index]
+	j = i + 1
+	
+	for j in xrange(i+1, len(alist)):
+		if alist[j] > pivot:
+			i = i +1
+			swap(alist, i, j)
+	
+	swap(alist, i + 1, right_index)
+	return i+1
+
+def swap(alist, i, j):
+	tmp = alist[i]
+	alist[i] = alist[j]
+	alist[j] = tmp
+	
