3e61449580052b45d128c3946b956d0c7a462e48
JoseIvanCC <jose.ivan.junior@ccc.ufcg.edu.br>
2018-11-06 17:13:07 -0300

    implementaçaõ do algoritmo guloso para o problema do troco das moedas.

--- atal/algorithms.py
+++ atal/algorithms.py
@@ -18,7 +18,7 @@ def retorna_matriculas_decrescente(alist):
 # Assuma que existe uma quantidade infinita de cada tipo de moeda
 def retorna_minimo_moedas(valor, tipos_moedas):
 	print valor, tipos_moedas
-	resultado = retorna_minimo_moedas_backtracking(tipos_moedas, valor)
+	resultado = troco_guloso(tipos_moedas, valor)
 	if resultado == sys.maxint:
 		return -1
 	else:
@@ -27,16 +27,16 @@ def retorna_minimo_moedas(valor, tipos_moedas):
 def retorna_minimo_moedas_FB(tipos_moedas, valor):
 	if valor == 0:
 		return 0
-	
+
 	resultado = sys.maxint
-	
+
 	for moeda in tipos_moedas:
-		
+
 		if (moeda <= valor):
-			resultado = min(resultado, retorna_minimo_moedas_FB(tipos_moedas, valor - moeda) + 1) 
-		
-	return resultado	
-	
+			resultado = min(resultado, retorna_minimo_moedas_FB(tipos_moedas, valor - moeda) + 1)
+
+	return resultado
+
 def retorna_minimo_moedas_backtracking(tipos_moedas, valor):
 	return retorna_minimo_moedas_BT(tipos_moedas, valor, [])
 
@@ -57,25 +57,35 @@ def retorna_minimo_moedas_BT(tipos_moedas, valor, possivel_solucao):
 			possivel_solucao.pop()
 	return tamanho_solucao
 
-#		
+#
 def eh_solucao(valor, moedas):
 	soma = 0
 	for i in range(len(moedas)):
 		soma = soma + moedas[i]
 	return soma == valor
-	
+
 def eh_solucao_promissora(possivel_solucao, valor):
 	soma = 0
 	for i in range(len(possivel_solucao)):
 		soma = soma + possivel_solucao[i]
 	return soma <= valor
 
-print retorna_minimo_moedas(30, [5,10,15])
-	
-	
+#print retorna_minimo_moedas(30, [5,10,15])
 
-	
+# SOLUCAO GULOSA
+def troco_guloso(moedas, valor):
+	possivel_solucao = []
+	value = 0
+	while (not eh_solucao(valor, possivel_solucao)):
+		if (not moedas):
+			return -1
+		valor_da_vez = max(moedas, key=int)
+		possivel_solucao.append(valor_da_vez)
+		if (eh_solucao_promissora(possivel_solucao, valor)):
+			value = value + 1
+		else:
+			possivel_solucao.pop()
+			moedas.remove(valor_da_vez)
+	return value
 
-	
-
-
+print troco_guloso([1,4,2], 5)
